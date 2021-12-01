64990576cffaf3ea7e454d3a6a01e0d776071b4d
SeungHyunH <vkflek1232@naver.com>
2021-12-01 12:51:12 +0900

    🏗 싱글톤 패턴 적용

--- app/src/main/java/kr/ac/kumoh/s20161271/gunplaapplication/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20161271/gunplaapplication/MainActivity.kt
@@ -2,14 +2,54 @@ package kr.ac.kumoh.s20161271.gunplaapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
 import kr.ac.kumoh.s20161271.gunplaapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    companion object {
+        const val QUEUE_TAG = "VolleyRequest"
+    }
+
+    private lateinit var mQueue: RequestQueue
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mQueue = VolleyRequest.getInstance(this.applicationContext).requestQueue
+        requestMechanic()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mQueue.cancelAll(QUEUE_TAG)
+    }
+
+    private fun requestMechanic() {
+        val url = "https://dbex-volley-server-cnzqf.run.goorm.io/gunpladb/mechanic/"
+
+        val request = JsonArrayRequest(
+            Request.Method.GET,
+            url,
+            null,
+            {
+                binding.result.text = it.toString()
+            },
+            {
+                Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
+                binding.result.text = it.toString()
+            }
+        )
+
+        request.tag = QUEUE_TAG
+        mQueue.add(request)
     }
 }
