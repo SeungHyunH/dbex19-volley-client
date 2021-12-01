baedef3d5c41f970115d4e1e56099f8e249965e8
SeungHyunH <vkflek1232@naver.com>
2021-12-01 13:09:48 +0900

    🎨 ViewModel Volley 성공

--- app/src/main/java/kr/ac/kumoh/s20161271/gunplaapplication/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20161271/gunplaapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package kr.ac.kumoh.s20161271.gunplaapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.viewModels
 import com.android.volley.Request
 import com.android.volley.RequestQueue
 import com.android.volley.toolbox.JsonArrayRequest
@@ -11,45 +12,13 @@ import com.android.volley.toolbox.Volley
 import kr.ac.kumoh.s20161271.gunplaapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        const val QUEUE_TAG = "VolleyRequest"
-    }
-
-    private lateinit var mQueue: RequestQueue
 
     private lateinit var binding: ActivityMainBinding
-
+    private val model : GunplaViewModel by viewModels()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        mQueue = VolleyRequest.getInstance(this.applicationContext).requestQueue
-        requestMechanic()
-    }
-
-    override fun onStop() {
-        super.onStop()
-        mQueue.cancelAll(QUEUE_TAG)
-    }
-
-    private fun requestMechanic() {
-        val url = "https://dbex-volley-server-cnzqf.run.goorm.io/gunpladb/mechanic/"
-
-        val request = JsonArrayRequest(
-            Request.Method.GET,
-            url,
-            null,
-            {
-                binding.result.text = it.toString()
-            },
-            {
-                Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
-                binding.result.text = it.toString()
-            }
-        )
-
-        request.tag = QUEUE_TAG
-        mQueue.add(request)
+        model.requestMechanic()
     }
 }
